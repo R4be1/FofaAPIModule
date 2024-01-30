eef415c4cb2df203b12fbfe3100c0d928235bae3
Rebel <110738599+R4be1@users.noreply.github.com>
2024-01-30 12:57:35 +0800

    Update fofaapi.py

--- fofaapi.py
+++ fofaapi.py
@@ -3,7 +3,8 @@ import base64
 import sys
 
 def FofaAPICall(query, email, api_key):
-    fields = 'host,ip,port,protocol,country_name,title,icp,country'
+    fields = ['protocol','host','ip','port','country_name','title','icp','country']
+    fields = ",".join(fields)
     qbase64 = base64.b64encode(query.encode()).decode()
     api = f'https://fofa.info/api/v1/search/all?email={email}&key={api_key}&qbase64={qbase64}&size=10000&fields={fields}'
 
@@ -14,7 +15,6 @@ def FofaAPICall(query, email, api_key):
 
     try:
         results = response.json().get("results",list())
-        #print(results)
         print("\033[38;2;24;254;27m[ {} ]\033[0m Fofa API Search {} Results.".format(query, len(results)))
 
     except Exception as error :
@@ -23,22 +23,25 @@ def FofaAPICall(query, email, api_key):
 
     hosts = list()
     file_name = "{}.csv".format(query.replace(" ","_"))
+    urls_file = open("urlresults.txt","w")
 
     with open(file_name,"a") as results_file:
         results_file.write(fields+"\n")
 
         for result in results:
-            hosts.append(
-                    result[0].strip()
-                    )
-            results_file.write(
-                    ",".join(result)+'\n'
-                    )
+            url = result[0].strip() + '://' +  result[1].strip() if '://' not in result[1] else result[1].strip()
+            print(",".join(result))
+            hosts.append( url )
+            urls_file.write( url+'\n' )
+            results_file.write( ",".join(result)+'\n' )
+
+        urls_file.close()
+
     return hosts
 
 if __name__=="__main__":
     FofaAPICall(
             query = sys.argv[1],
-            email = None,
-            api_key = None
+            email='',
+            api_key=''
             )
